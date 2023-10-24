10a52a18c3c994ba585f78808f8970e76945329d
parwez-zafar <parwezzafar3@gmail.com>
2023-10-24 09:12:45 +0530

    changes name to device id and validated

--- models/userModel.js
+++ models/userModel.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: [true, "Please Enter Your Name"],
+        required: [true, "Please Enter Your Device Id"],
+        maxLenght: [10, "Id should be 10 digit number"],
+        minLength: [10, "Id should be 10 digit number"]
 
     },
     username: {
